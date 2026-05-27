1fa68986eb9f218e8f17047ecdf25ffb238a2943
kevinjaffe <kevinjaffe@hotmail.com>
2026-05-27 17:16:13 +0000

    Tablas DB

--- backend/pichon_db.sql
+++ backend/pichon_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql:3306
--- Generation Time: May 26, 2026 at 08:17 PM
+-- Generation Time: May 27, 2026 at 05:15 PM
 -- Server version: 8.0.46
 -- PHP Version: 8.3.31
 
@@ -20,6 +20,43 @@ SET time_zone = "+00:00";
 --
 -- Database: `pichon_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat`
+--
+
+CREATE TABLE `chat` (
+  `id_chat` int NOT NULL,
+  `id_usuario1` int NOT NULL,
+  `id_usuario2` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contactos`
+--
+
+CREATE TABLE `contactos` (
+  `id_usuario` int NOT NULL,
+  `id_contacto` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mensaje`
+--
+
+CREATE TABLE `mensaje` (
+  `id_mensaje` int NOT NULL,
+  `id_chat` int NOT NULL,
+  `id_usuario` int NOT NULL,
+  `contenido` text NOT NULL,
+  `fecha` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -47,6 +84,29 @@ INSERT INTO `usuario` (`id`, `username`, `password`, `alias`, `img`) VALUES
 --
 
 --
+-- Indexes for table `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`id_chat`),
+  ADD KEY `fk_usuario1` (`id_usuario1`),
+  ADD KEY `fk_usuario2` (`id_usuario2`);
+
+--
+-- Indexes for table `contactos`
+--
+ALTER TABLE `contactos`
+  ADD PRIMARY KEY (`id_usuario`,`id_contacto`),
+  ADD KEY `fk_contacto` (`id_contacto`);
+
+--
+-- Indexes for table `mensaje`
+--
+ALTER TABLE `mensaje`
+  ADD PRIMARY KEY (`id_mensaje`),
+  ADD KEY `fk_chat` (`id_chat`),
+  ADD KEY `fk_usuario_mensaje` (`id_usuario`);
+
+--
 -- Indexes for table `usuario`
 --
 ALTER TABLE `usuario`
@@ -57,10 +117,47 @@ ALTER TABLE `usuario`
 --
 
 --
+-- AUTO_INCREMENT for table `chat`
+--
+ALTER TABLE `chat`
+  MODIFY `id_chat` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `mensaje`
+--
+ALTER TABLE `mensaje`
+  MODIFY `id_mensaje` int NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `chat`
+--
+ALTER TABLE `chat`
+  ADD CONSTRAINT `fk_usuario1` FOREIGN KEY (`id_usuario1`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_usuario2` FOREIGN KEY (`id_usuario2`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `contactos`
+--
+ALTER TABLE `contactos`
+  ADD CONSTRAINT `fk_contacto` FOREIGN KEY (`id_contacto`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `mensaje`
+--
+ALTER TABLE `mensaje`
+  ADD CONSTRAINT `fk_chat` FOREIGN KEY (`id_chat`) REFERENCES `chat` (`id_chat`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_usuario_mensaje` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
