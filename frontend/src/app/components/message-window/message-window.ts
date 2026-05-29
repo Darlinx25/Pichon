e20dc6270f31f17e77c80583a6798139e6682dbe
mizu43 <ignacio.ortega@estudiantes.utec.edu.uy>
2026-05-29 18:31:20 -0300

    campo enviar mensaje base

--- frontend/src/app/components/message-window/message-window.ts
+++ frontend/src/app/components/message-window/message-window.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-message-window',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './message-window.html',
   styleUrl: './message-window.scss',
 })
-export class MessageWindow {}
+export class MessageWindow {
+  formBuilder = inject(FormBuilder);
+
+  chatForm = this.formBuilder.group({
+    mensaje: ['', Validators.required]
+  });
+
+  enviarMensaje() {
+    
+  }
+}
