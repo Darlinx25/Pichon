4678cbde591db1993f5ab2ae89b7fffeb5458e17
Darlinx25 <fsalaberry505@gmail.com>
2026-05-31 19:32:52 -0300

    Cambio puerto SQL

--- backend/db.php
+++ backend/db.php
@@ -1,9 +1,10 @@
 <?php
 $servidor = "127.0.0.1";
+$puerto = 3307;
 $usuario = "pichon_user";
 $contrasena = "pichon_pass";
 $base_datos = "pichon_db";
-$conexion = mysqli_connect($servidor, $usuario, $contrasena, $base_datos);
+$conexion = mysqli_connect($servidor, $usuario, $contrasena, $base_datos, $puerto);
 if (!$conexion) {
     http_response_code(500);
     echo json_encode(["error" => "Error de conexión: " . mysqli_connect_error()]);
---
+++ docker/mysql/init.sql
@@ -0,0 +1,2 @@
+ALTER USER 'pichon_user'@'%' IDENTIFIED WITH mysql_native_password BY 'pichon_pass';
+FLUSH PRIVILEGES;
