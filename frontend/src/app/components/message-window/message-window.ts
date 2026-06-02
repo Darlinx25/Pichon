19b43f0a24c88f94340e89e3b83af7b22e5b74af
mizu43 <ignacio.ortega@estudiantes.utec.edu.uy>
2026-06-02 16:23:17 -0300

    sonido notificación de mensajes

--- frontend/src/app/components/message-window/message-window.ts
+++ frontend/src/app/components/message-window/message-window.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnDestroy, OnInit,ChangeDetectorRef, AfterViewChecked, ViewChild, ElementRef } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, ChangeDetectorRef, AfterViewChecked, ViewChild, ElementRef } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink, Router } from '@angular/router';
 import { Sidebar } from '../sidebar/sidebar'
@@ -13,29 +13,31 @@ import { Websocket } from '../../services/websocketService';
 })
 
 
-export class MessageWindow implements OnInit, OnDestroy  {
+export class MessageWindow implements OnInit, OnDestroy {
   messages: any[] = [];
   private messageSubscription!: Subscription;
+  private sonidoNotificacion = new Audio('assets/notification.mp3');
 
-  constructor(private webSocketService: Websocket,private cdr: ChangeDetectorRef) {}
+  constructor(private webSocketService: Websocket, private cdr: ChangeDetectorRef) { }
 
   formBuilder = inject(FormBuilder);
-  router = inject(Router); 
+  router = inject(Router);
 
   chatForm = this.formBuilder.group({
     mensaje: ['', Validators.required]
   });
 
   ngOnInit() {
-    if(!localStorage.getItem('user')){
+    if (!localStorage.getItem('user')) {
       this.router.navigate(['/']);
     }
 
     this.messageSubscription = this.webSocketService.getMessages().subscribe(
       (message) => {
-         this.messages.push({ ...message, side: 'incoming' });
-         //Se usa incoming u outgoing como abajo en enviar mensaje para definir de que lado se muestra el mensaje
-         this.cdr.detectChanges();
+        this.messages.push({ ...message, side: 'incoming' });
+        //Se usa incoming u outgoing como abajo en enviar mensaje para definir de que lado se muestra el mensaje
+        this.cdr.detectChanges();
+        this.playNotificacion();
       }
     );
 
@@ -75,5 +77,10 @@ export class MessageWindow implements OnInit, OnDestroy  {
   scrollearAbajo(): void {
     const element = this.msgContainer.nativeElement;
     element.scrollTop = element.scrollHeight;
+  }
+
+  playNotificacion(): void {
+    this.sonidoNotificacion.load();
+    this.sonidoNotificacion.play().catch(err => console.error('Erorr al reproducir audio:', err));
   }
 }
