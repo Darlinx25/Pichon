81544f74410d20e93e45c7314d0b3f7af6baf61a
mizu43 <ignacio.ortega@estudiantes.utec.edu.uy>
2026-06-03 19:47:17 -0300

    se ven los usuarios en busqueda

--- backend/buscarUsuarios.php
+++ backend/buscarUsuarios.php
@@ -13,7 +13,7 @@ $conexion = require "./db.php";
 $search = $_GET['search'] ?? '';
 
 $query = $conexion->prepare(
-    "SELECT id, alias
+    "SELECT id, alias, img
      FROM usuario
      WHERE alias LIKE CONCAT('%', ?, '%')
      LIMIT 30"
--- frontend/src/app/components/sidebar/sidebar.ts
+++ frontend/src/app/components/sidebar/sidebar.ts
@@ -1,9 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from '../../../environments/environment';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { UserService } from '../../services/user-service';
-import { debounceTime, distinctUntilChanged, filter, switchMap } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, of, switchMap } from 'rxjs';
 
 
 
@@ -14,11 +14,11 @@ import { debounceTime, distinctUntilChanged, filter, switchMap } from 'rxjs';
   styleUrl: './sidebar.scss',
 })
 export class Sidebar {
-  router = inject(Router);  
-  user = JSON.parse(localStorage.getItem('user') || '{}'); 
+  router = inject(Router);
+  user = JSON.parse(localStorage.getItem('user') || '{}');
   apiBaseUrl = environment.apiBaseUrl;
 
-  cerrarSesion(){
+  cerrarSesion() {
     localStorage.removeItem('user');
     this.router.navigate(['/']);
   }
@@ -31,20 +31,33 @@ export class Sidebar {
     busqueda: ['']
   });
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private cdr: ChangeDetectorRef) { }
 
   ngOnInit() {
     this.searchForm.get('busqueda')?.valueChanges
       .pipe(
         debounceTime(300),
         distinctUntilChanged(),
-        filter(texto => (texto?.trim().length ?? 0) >= 2),
-        switchMap(texto =>
-          this.userService.buscarUsuarios(texto ?? '')
-        )
+        switchMap(texto => {
+          if (!texto || texto.trim().length < 2) {
+            return of([]);
+          }
+
+          return this.userService.buscarUsuarios(texto);
+        })
       )
       .subscribe(usuarios => {
         this.usuariosEncontrados = usuarios;
+        this.cdr.detectChanges();
       });
+  }
+
+  seleccionarUsuario(usuario: any) {
+    console.log(usuario);
+
+    //ver cómo pasar a campo del chat luego de clickear
+    //y como ponerlo a la izquierda en chats "activos"
+
+    this.usuariosEncontrados = [];
   }
 }
