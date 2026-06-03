1943cb0a534ba07acd68162daa177662adfa17d6
mizu43 <ignacio.ortega@estudiantes.utec.edu.uy>
2026-06-03 12:54:34 -0300

    buscar usuarios wip

--- frontend/src/app/components/sidebar/sidebar.ts
+++ frontend/src/app/components/sidebar/sidebar.ts
@@ -1,11 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from '../../../environments/environment';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { UserService } from '../../services/user-service';
+import { debounceTime, distinctUntilChanged, filter, switchMap } from 'rxjs';
+
 
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
@@ -17,5 +21,30 @@ export class Sidebar {
   cerrarSesion(){
     localStorage.removeItem('user');
     this.router.navigate(['/']);
+  }
+
+  private formBuilder = inject(FormBuilder);
+
+  usuariosEncontrados: any[] = [];
+
+  searchForm = this.formBuilder.group({
+    busqueda: ['']
+  });
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.searchForm.get('busqueda')?.valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        filter(texto => (texto?.trim().length ?? 0) >= 2),
+        switchMap(texto =>
+          this.userService.buscarUsuarios(texto ?? '')
+        )
+      )
+      .subscribe(usuarios => {
+        this.usuariosEncontrados = usuarios;
+      });
   }
 }
---
+++ frontend/src/app/services/user-service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  buscarUsuarios(texto: string) {
+    return this.http.get<any[]>(
+      `${environment.apiBaseUrl}/buscarUsuarios.php?search=${encodeURIComponent(texto)}`
+    );
+  }
+}
