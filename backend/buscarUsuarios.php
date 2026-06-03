8023a64be64f4a75e8b53c46ac07ef766a66d9de
mizu43 <ignacio.ortega@estudiantes.utec.edu.uy>
2026-06-03 13:01:17 -0300

    buscarUsuarios.php

---
+++ backend/buscarUsuarios.php
@@ -0,0 +1,34 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit;
+}
+$conexion = require "./db.php";
+
+$search = $_GET['search'] ?? '';
+
+$query = $conexion->prepare(
+    "SELECT id, alias
+     FROM usuario
+     WHERE alias LIKE CONCAT('%', ?, '%')
+     LIMIT 30"
+);
+
+$query->bind_param("s", $search);
+$query->execute();
+
+$resultado = $query->get_result();
+
+$users = [];
+
+while ($row = $resultado->fetch_assoc()) {
+    $users[] = $row;
+}
+
+header('Content-Type: application/json');
+echo json_encode($users);
