6e819691df6bb0e84550b7a5b5297ba628156700
Darlinx25 <fsalaberry505@gmail.com>
2026-06-17 10:11:11 -0300

    Reconexion automatica

--- frontend/src/app/services/websocketService.ts
+++ frontend/src/app/services/websocketService.ts
@@ -6,25 +6,45 @@ import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class Websocket {
-  private socket$: WebSocketSubject<any>;
+  private socket$: WebSocketSubject<any> | null = null;
   private messagesSubject = new Subject<any>();
+  private currentUserId: number | null = null;
+
   constructor(private ngZone: NgZone) {
+    this.connect();
+  }
+
+  private connect() {
     this.socket$ = webSocket(environment.wsUrl);
     this.socket$.subscribe({
       next: (msg) => this.ngZone.run(() => this.messagesSubject.next(msg)),
-      error: (err) => console.error('[WS] Error:', err),
+      error: (err) => {
+        console.error('[WS] Error:', err);
+        this.reconnect();
+      },
+      complete: () => this.reconnect(),
     });
   }
+
+   private reconnect() {
+    setTimeout(() => {
+      this.connect();
+      if (this.currentUserId) {
+        this.socket$?.next({ type: 'auth', userId: this.currentUserId });
+      }
+    }, 3000);
+  }
+
   sendMessage(message: any) {
-    this.socket$.next(message);
+    this.socket$?.next(message);
   }
   getMessages(): Observable<any> {
     return this.messagesSubject.asObservable();
   }
   closeConnection() {
-    this.socket$.complete();
+    this.socket$?.complete();
   }
   authenticate(userId: number) {                    
-    this.socket$.next({ type: 'auth', userId });
+    this.socket$?.next({ type: 'auth', userId });
   }
 }
