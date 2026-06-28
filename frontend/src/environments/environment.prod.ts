873bebaba90973e57e328e2dd2243effe465fe0e
Darlinx25 <fsalaberry505@gmail.com>
2026-06-28 14:57:47 -0300

    build prod ip dinamica y apache sirve frontend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  wsUrl: 'ws://192.168.1.24:8082/ws',
-  apiBaseUrl: 'http://192.168.1.24:8082/backend'
+  wsUrl: `${window.location.protocol === 'https:' ? 'wss:' : 'ws:'}//${window.location.host}/ws`,
+  apiBaseUrl: `${window.location.origin}/backend`
 };
